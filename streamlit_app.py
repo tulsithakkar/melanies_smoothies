f9c535399435ab39042f5962a7d12d1bb1a43bec
Tulsi Thakkar <100224177+tulsithakkar@users.noreply.github.com>
2024-09-13 15:02:02 +0530

    Update streamlit_app.py
    change the type of response

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,6 @@ if ingredients_list:
         
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
